32251419788c0379809dad85848c18f735420256
Brice Allard <briceallard@gmail.com>
2017-10-24 01:44:41 -0500

    Update allard_brice_prog2.cpp

--- Program 2 - Recurrence Relations/allard_brice_prog2.cpp
+++ Program 2 - Recurrence Relations/allard_brice_prog2.cpp
@@ -1,38 +1,80 @@
+/*
+Brice Allard
+10/23/2017
+Recurrence Relations - Program 2
+	// Show understanding of concepts of solving recurrence
+	// relations, and comparing recursive and non-recursive
+	// solutions
+*/
+
 #include <iostream>
 #include <fstream>
 #include <cmath>
 
 using namespace std;
 
-int recurrenceR(int k, int m, int a_, int n) {
-	int total = 0;
-	return total;
+// Recurrence function
+int recurrenceR(int k, int m, int a_, int n, int count) {
+	int answer = a_;	// stores answer of each relation
+
+	// Checks if we've reached the 'n'th relation
+	if (count == n)
+		return answer;	// if so, returns answer
+	else {
+		a_ = (k * a_) + m;	// if not, solves relation
+	}
+
+	count++;
+	recurrenceR(k, m, a_, n, count);	// recurrence here
 }
 
+// Non-Recurrence function
 int recurrenceNR(int k, int m, int a_, int n) {
-	int total = 0;
-	return total;
+	// implement array to store values from each relation
+	int* arr = NULL;
+	arr = new int[n];
+	arr[0] = a_;
+
+	for (int i = 1; i <= n; i++) {
+		arr[i] = (k * arr[i - 1]) + m;	// solves relation
+	}
+
+	return arr[n];
 }
 
 int main() {
+	// create infile/outfile
 	ifstream infile;
 	infile.open("recur.dat");
 	ofstream outfile;
 	outfile.open("allard_brice_recur.out");
 
+	// set member variables
 	int k, m, a_, n;
-	int count = 1;
+	int equation = 1;
+	int count = 0;
+
+	// read in from infile and store in variables
 	infile >> k >> m >> a_ >> n;
 
+	cout << "Brice Allard" << "\n\n";
+	outfile << "Brice Allard" << "\n\n";
+
+	// do till end of file
 	while (!infile.eof()) {
-		cout << "Equation #" << count << "\n\n";
+		cout << "Equation #" << equation << "\n\n";
+		outfile << "Equation #" << equation << "\n\n";
 		cout << "The " << n << " term found using the recursive solution is: "
-			<< recurrenceR(k, m, a_, n) << '\n';
-		cout << "The " << n << " term found using the recursive solution is: "
+			<< recurrenceR(k, m, a_, n, count) << '\n';
+		outfile << "The " << n << " term found using the recursive solution is: "
+			<< recurrenceR(k, m, a_, n, count) << '\n';
+		cout << "The " << n << " term found using the non-recursive solution is: "
+			<< recurrenceNR(k, m, a_, n) << "\n\n";
+		outfile << "The " << n << " term found using the non-recursive solution is: "
 			<< recurrenceNR(k, m, a_, n) << "\n\n";
 
-		infile >> k >> m >> a_ >> n;
-		count++;
+		infile >> k >> m >> a_ >> n;	// update variables
+		equation++;
 	}
 
 	system("pause");
